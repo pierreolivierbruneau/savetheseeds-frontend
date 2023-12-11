e7684793a42f4d985bae93af75471b5f12d0c656
Veronique Azzopardi <azzopardivero@gmail.com>
2023-12-11 16:21:39 +0100

    fixed home js conflicts

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,10 @@
 import styles from "../styles/Home.module.css";
+import Header from "./Header";
 
 function Home() {
   return (
     <div>
+      <Header />
       <main className={styles.main}>
         <h1 className={styles.title}></h1>
 
