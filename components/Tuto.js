7bd0ad7e441d4d8ef42adbbaa28a7b9f3d1d3eda
Veronique Azzopardi <azzopardivero@gmail.com>
2023-12-12 11:34:46 +0100

    probleme de la navbar reglé

---
+++ components/Tuto.js
@@ -0,0 +1,13 @@
+import styles from "../styles/Tutos.module.css";
+
+function Tuto() {
+  return (
+    <div>
+      <main className={styles.main}>
+        <h3 className={styles.title}>Vous Allez Créer Vos Semis</h3>
+      </main>
+    </div>
+  );
+}
+
+export default Tuto;
