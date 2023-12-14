c414c65939dffc406b73ab34e0933c54257edd41
Kayla <kaylaljacks@hotmail.com>
2023-12-14 15:56:34 +0100

    connected mongoose

--- components/Forum2.js
+++ components/Forum2.js
@@ -1,7 +1,19 @@
 import styles from '../styles/Forum2.module.css';
+import {useState} from "react";
+
 
 function Forum2(){
+const[comment, setComment] = useState("");
+const[comments, setComments] = useState([]);
 
+
+const onClickHandler = () =>{
+    setComments((comments) => [...comments,comment]);
+
+}
+const onChangeHandler = (e) => {
+setComment(e.target.value);
+}
 
     return(
 <div>
@@ -21,14 +33,29 @@ function Forum2(){
 
 <div>
     <div className={styles.text2}>Contenu du poste</div>
-    <input type="text" className={styles.textbox2}></input>
+    <input type="text" 
+    value={comment} 
+    onChange={onChangeHandler} 
+    className={styles.textbox2}>
+
+    </input>
 </div>
 
 
 </div>
 
 
-<button type="submit" className={styles.submit}>Submit</button>
+<button 
+type="submit" 
+onClick={onClickHandler} 
+className={styles.submit}>
+    Submit
+    </button>
+
+
+{comments.map((text) =>(
+    <div className ={styles.commentContainer}> {text} </div>
+))}
 
 
 </div>
