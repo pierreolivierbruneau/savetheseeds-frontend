a9a2442d6b9903e94d20970a6fc1d4cab0a1c8e3
pierreolivierbruneau <pierreolivierbruneau@gmail.com>
2023-12-19 15:16:09 +0100

    mapzone

--- components/Map.js
+++ components/Map.js
@@ -1,35 +1,54 @@
-import { MapContainer, TileLayer, Polygon, Rectangle, Popup, Marker, useMapEvents, MapConsumer } from 'react-leaflet'
+import { MapContainer, TileLayer, Polygon, Rectangle, Popup, Circle, Polyline, Marker, useMapEvents, MapConsumer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import { useState } from 'react';
 import icon from "./Marker";
 import L from "leaflet";
 
 function Map() {
+  const center = [43.705836896399994, 7.260651102906279]
+  const center1 = [43.08399379118767, 5.932892008428375]
+  const center2 = [43.28894741645583, 5.39491390945662]
+  const center3 = [43.554710332836414, 7.018404673047523]
 
   const latitude = 43.66922946632716;
   const longitude = 7.2139079502649235;
 
-  const polyline = [
+  const polygon = [
     [
-      [43.6479594905745, 7.208245272085059],
-      [43.66744687167811, 7.230609345325904],
-      [43.683676162803856, 7.196566431264706],
+      [43.85026517039784, 7.452355869610164],
+      [43.69662044797769, 7.560715532049445],
+      [43.11894607648215, 6.1648365026287895],
     ],
 
     [
-      [43.689769097513064, 7.239135193367816],
-      [43.69633461618219, 7.281424997110655],
-      [43.7266044622628, 7.2541545195247314],
+      [43.3441193381045, 6.6702304422581165],
+      [43.030182487541, 6.166669888802283],
+      [43.41783330612997, 5.301400658762429],
     ],
   ]
   const rectangle = [
-    [43.742709102769744, 7.180482878493162],
-    [43.68207804444842, 7.191297443856154],
+    [44.8098755196583, 6.904082913427415],
+    [43.79345263697241, 5.229380791000217],
   ]
+
+
+  const multiPolyline = [
+ 
+  [
+    [43.747590156668394, 5.513841201961662],
+    [43.56033366797934, 5.459000649524677],
+    [43.42192996440685, 6.423914968253879],
+    [43.79186727778059, 7.16983260191406],
+    [43.747590156668394, 5.513841201961662],
+  ],
+ 
+]
+
 
   const redOptions = { color: 'red' }
   const greenOptions = { color: 'green' }
   const fillBlueOptions = { fillColor: 'blue' }
+  const purpleOptions = { color: 'purple' }
 
 
   return (
@@ -52,14 +71,20 @@ function Map() {
 
         });
       }}>
-      <div style={{ position: "absolute", height: "150px", width: "300px", backgroundColor: "#ffffffc4", padding: "15px", zIndex: 1000, bottom: 0, right: "10px", borderRadius: "20px" }}>
+      <div style={{ position: "absolute", height: "200px", width: "400px", backgroundColor: "#ffffffc4", padding: "15px", zIndex: 1000, bottom: 0, right: "10px", borderRadius: "20px" }}>
 
         <ul>
           <li style={{ color: "red" }}>
-                      Zone où planter
+          Zone où il est interdit de planter
           </li>
           <li style={{ color: "green" }}>
-            Zone où il est interdit de planter
+            Zone où planter des pommier pour son altitude, son climat favorable, la réduction des maladies et l'eau de qualité
+          </li>
+          <li style={{ color: "blue" }}>
+            Zone ou planter des rosacées pour son microclimat, la résistance au sel et la pollinisation
+          </li>
+          <li style={{ color: "purple" }}>
+            Zone ou planter des Lauracées
           </li>
           <li>
             <img src="https://unpkg.com/leaflet@1.7/dist/images/marker-icon.png"></img>la ou j'ai planter
@@ -71,9 +96,21 @@ function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-      <Polygon pathOptions={redOptions} positions={polyline} />
+      <Polygon pathOptions={fillBlueOptions} positions={polygon}/>
       <Rectangle bounds={rectangle} pathOptions={greenOptions} />
-
+      <Polyline pathOptions={purpleOptions} positions={multiPolyline} />
+      <Circle center={center} pathOptions={redOptions} radius={3000} >
+        <Popup>zone ou planter ou il est interdit de planter</Popup>
+      </Circle>
+      <Circle center={center1} pathOptions={redOptions} radius={5000} >
+        <Popup>zone ou planter ou il est interdit de planter</Popup>
+      </Circle>
+      <Circle center={center2} pathOptions={redOptions} radius={5000} >
+        <Popup>zone ou planter ou il est interdit de planter</Popup>
+      </Circle>
+      <Circle center={center3} pathOptions={redOptions} radius={5000} >
+        <Popup>zone ou planter ou il est interdit de planter</Popup>
+      </Circle>
     </MapContainer>
   );
 }
