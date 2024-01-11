6c21c6fbbc1c637d857fc5f010b26a3bc7d108c5
pierreolivierbruneau <pierreolivierbruneau@gmail.com>
2024-01-11 11:17:42 +0100

    Profil

--- components/Profil.js
+++ components/Profil.js
@@ -1,4 +1,4 @@
-import styles from "../styles/Profil.module.css";
+import styles from "../styles/PROFIL.module.css";
 
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
